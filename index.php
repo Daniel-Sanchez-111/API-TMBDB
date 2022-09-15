00d93d401e4e03e2c8041282cf33496985b24f90
Daniel-Sanchez-111 <62917581+Daniel-Sanchez-111@users.noreply.github.com>
2022-09-15 09:57:07 -0600

    LESS prueba

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Acceso al panel</title>
+	<link href="public/css/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container">
+		<form>
+			<fieldset>
+				<legend>
+					Datos de acceso
+				</legend>
+				<label>
+					Email:
+				</label>
+				<input type="text" name="">
+				<label>
+					Password;
+				</label>
+				<input type="password" name="">
+				<button type="submit">
+					Acceder
+				</button>
+			</fieldset>
+		</form>
+	</div>
+</body>
+</html>
